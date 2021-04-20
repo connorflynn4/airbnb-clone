7c0e3ea2b6a38ce04b29a3bf33daa07c0c44c9a2
Connor Flynn <connorflynn4@gmail.com>
2021-04-19 20:21:20 -0700

    final commit

--- src/Header.js
+++ src/Header.js
@@ -4,15 +4,19 @@ import SearchIcon from '@material-ui/icons/Search';
 import LanguageIcon from '@material-ui/icons/Language';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { Avatar } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 function header() {
     return (
         <div className="header">
-        <img
+        <Link to='/'>
+            <img
             className="header__icon"
             src="https://cdn.designrush.com/uploads/inspiration_images/4810/990__1511452487_364_Airbnb.png"
             alt=""
         />
+        </Link>
+       
 
         <div className="header__center">
             <input type="text" />
